4e601023129669561191b432061a06fe086c440f
Mostafa Moradian <mostafamoradian0@gmail.com>
2020-09-19 15:09:10 +0200

    Replaced single with double-quotes

--- lib/jekyll-resize.rb
+++ lib/jekyll-resize.rb
@@ -1,6 +1,6 @@
-require "open-uri"
+require 'open-uri'
 require 'digest'
-require "mini_magick"
+require 'mini_magick'
 
 module Jekyll
   module Resize
@@ -12,9 +12,9 @@ module Jekyll
       end
 
       source_path = site.source + source
-      raise "#{source_[path]} is not readable" unless File.readable?(source_path)
+      raise '#{source_[path]} is not readable' unless File.readable?(source_path)
 
-      destination_path = "/cache/resize/"
+      destination_path = '/cache/resize/'
       destination = site.source + destination_path
 
       FileUtils.mkdir_p destination
@@ -24,12 +24,12 @@ module Jekyll
 
       sha = Digest::SHA256.file source_path
 
-      destination_file_name = "#{sha}_#{desc}#{ext}"
+      destination_file_name = '#{sha}_#{desc}#{ext}'
       destination += destination_file_name
 
       if !File.exist?(destination) || File.mtime(destination) <= File.mtime(source_path)
 
-        puts "Thumbnailing #{source_path} to #{destination} (#{options})"
+        puts 'Thumbnailing #{source_path} to #{destination} (#{options})'
 
         image = MiniMagick::Image.open(source_path)
         image.strip
@@ -44,7 +44,7 @@ module Jekyll
     def download(source)
       filename = File.basename(url)
 
-      destination_path = "/cache/downloads/"
+      destination_path = '/cache/downloads/'
       destination = site.source + destination_path
       destination_file_name = destination_path + filename
 
@@ -54,8 +54,8 @@ module Jekyll
 
       if !File.exist?(destination_file_name)
         URI.open(url) do |image|
-          File.open(destination, "wb") do |file|
-            puts "Downloading #{filename} to #{destination_file_name}"
+          File.open(destination, 'wb') do |file|
+            puts 'Downloading #{filename} to #{destination_file_name}'
             file.write(image.read)
           end
         end
