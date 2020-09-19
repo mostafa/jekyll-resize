d249b566fc97748ff35c779165b3a362ac969992
Mostafa Moradian <mostafamoradian0@gmail.com>
2020-09-19 14:54:26 +0200

    Add implicit download if the given image is a URL (before resizing)

--- lib/jekyll-resize.rb
+++ lib/jekyll-resize.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 require 'digest'
 require "mini_magick"
 
@@ -5,6 +6,10 @@ module Jekyll
   module Resize
     def resize(source, options)
       site = @context.registers[:site]
+
+      if !File.exist?(source)
+        download(source)
+      end
 
       source_path = site.source + source
       raise "#{source_[path]} is not readable" unless File.readable?(source_path)
@@ -34,6 +39,28 @@ module Jekyll
       end
 
       destination_path + destination_file_name
+    end
+
+    def download(source)
+      filename = File.basename(url)
+
+      destination_path = "/cache/downloads/"
+      destination = site.source + destination_path
+      destination_file_name = destination_path + filename
+
+      if !Dir.exists?(destination)
+        FileUtils.mkdir_p destination
+      end
+
+      if !File.exist?(destination_file_name)
+        URI.open(url) do |image|
+          File.open(destination, "wb") do |file|
+            file.write(image.read)
+          end
+        end
+      end
+
+      return destination_file_name
     end
   end
 end
